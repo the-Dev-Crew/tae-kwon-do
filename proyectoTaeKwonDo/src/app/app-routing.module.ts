f05b0ec2aa36e66286d412566ad30fe9b8141159
Christellox <tello@ciencias.unam.mx>
2020-11-16 03:58:19 -0600

    tipo evento front aun incompleto

--- proyectoTaeKwonDo/src/app/app-routing.module.ts
+++ proyectoTaeKwonDo/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TipoEventoComponent} from './components/tipo-evento/tipo-evento.component';
+const routes: Routes = [
+  {path: '', component: TipoEventoComponent},
+  {path: 'tipo_evento', component: TipoEventoComponent},
 
-const routes: Routes = [];
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyectoTaeKwonDo/src/app/app.module.ts
+++ proyectoTaeKwonDo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipoEventoComponent } from './components/tipo-evento/tipo-evento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipoEventoComponent,
   ],
   imports: [
     BrowserModule,
