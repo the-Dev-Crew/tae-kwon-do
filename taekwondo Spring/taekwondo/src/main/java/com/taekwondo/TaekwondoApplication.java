10b28b8ac7193506cfe4dd5ba33a15d10c8970d8
Christellox <tello@ciencias.unam.mx>
2020-11-16 00:15:02 -0600

    se agrego carpeta de back

---
+++ taekwondo Spring/taekwondo/src/main/java/com/taekwondo/TaekwondoApplication.java
@@ -0,0 +1,13 @@
+package com.taekwondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaekwondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaekwondoApplication.class, args);
+	}
+
+}
